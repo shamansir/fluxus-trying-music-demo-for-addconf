9be49276c43507f41c764d3f42ff6fc8e3f0b069
Ulric Wilfred <shaman.sir@gmail.com>
2011-04-27 09:10:12 +0300

    getting required user info

--- collect-twitters.js
+++ collect-twitters.js
@@ -1,6 +1,19 @@
 var http = require('http');
+var json = JSON;
 
-var twi_api = function(method, params) {
+var OUTPUT = 'source.ssv';
+
+var users_to_get = [ 'shaman_sir',
+                     'digal',
+                     'crzcrz',
+                     'ennoel',
+                     'mikashkin',
+                     'mcm86',
+                     'muromec',
+                     'not_existent_one' ];
+
+// constructs http request options from api method and its params
+function twi_api(method, params) {
     return {
        host: 'api.twitter.com',
        port: 80,
@@ -9,15 +22,42 @@ var twi_api = function(method, params) {
     };
 }
 
-var req = http.get(
-    twi_api('users/show', 'screen_name=shaman_sir'),
-    function(res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
+// twitter api responses are streaming, so we need to accumulate them
+function acc_stream(res, callback) {
+    var body = "";
 
-        res.on('data', function(d) {
-            process.stdout.write(d);
+    res.on('data', function(chunk) {
+        body += chunk;
+    });
+
+    res.on('end', function() {
+        if (callback) callback(json.parse(body));
+    });
+}
+
+// =============================================================================
+// now the magic, requesting all data
+
+for (var i = 0; i < users_to_get.length; i++) {
+    var user = users_to_get[i];
+
+    console.log('Requesting data for user ' + user);
+    var req = http.get(
+        twi_api('users/show', 'screen_name=' + user),
+        (function(user) { return function(res) {
+            console.log('===================================');
+            console.log(user + ' / STATUS: ' + res.statusCode);
+        }})(user)
+    );
+
+    req.on('response', function(res) {
+        if (res.statusCode === 404) return;
+        acc_stream(res, function(user_info) {
+            console.log('Name: ' + user_info['screen_name']);
+            console.log('Geo enabled: ' + user_info['geo_enabled']);
+            console.log('Location: ' + user_info['location']);
         });
-    }
-);
+    });
+
+}
 
