1d0b55abb086779ff31c592c469be44c42d76278
Ulric Wilfred <shaman.sir@gmail.com>
2011-04-27 21:26:59 +0300

    user label

--- twi-users.scm
+++ twi-users.scm
@@ -1,17 +1,27 @@
+(clear)
+
+(define font "./DroidSans.ttf")
+
 ; twitter user structure
 (define-struct twi-user
-    (name screen_name followers_num
-     tweets_num followers tweets))
+    (name screen-name followers-num
+     tweets-num followers tweets
+     label))
+
+; quick text object
+(define (qto _text)
+    (build-extruded-type font _text .2))
 
 ; myself
 (define me (make-twi-user
         "shaman" "shaman_sir"
-        615 478 (list) (list)))
+        615 478 (list) (list)
+        (qto "shaman")))
 
 ; draw user function
 (define (draw-user _user)
     ; head
-    (with-state  
+    (with-state
         (colour (vector 1 0 0))
         (translate (vector 0 1 0))
         (scale 0.6)
@@ -20,7 +30,7 @@
         (colour (vector 1 1 1))
         (scale 0.2)
         (translate (vector 2 .2 6))
-        (draw-sphere)                
+        (draw-sphere)
         (translate (vector -4 0 0))
         (draw-sphere))
     ; body
@@ -51,8 +61,14 @@
         (translate (vector .2 -1.8 0))
         (scale (vector .3 1.2 .5))
         (draw-cube))
+    ; name
+    (with-primitive (twi-user-label _user)
+        (identity)
+        (translate (vector -1.3 -3.2 0))
+        (scale .15))
 )
 
 ; every frame
 (every-frame
     (draw-user me))
+
