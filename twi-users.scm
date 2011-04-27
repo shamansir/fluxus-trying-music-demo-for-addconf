4fc974c924fce39c2131292aa5ba70837194dd48
Ulric Wilfred <shaman.sir@gmail.com>
2011-04-27 20:13:04 +0300

    showing a user

--- twi-users.scm
+++ twi-users.scm
@@ -1,8 +1,34 @@
-(define user '("shaman" 220 340))
+; twitter user structure
+(define-struct twi-user
+    (name screen_name followers_num
+     tweets_num followers tweets))
 
-(define (draw-user user)
-    (colour (vector 1 0 0))
-    (draw-sphere)) 
+; myself
+(define me (make-twi-user
+        "shaman" "shaman_sir"
+        615 478 (list) (list)))
 
+; draw user function
+(define (draw-user _user)
+    ; head
+    (with-state  
+        (colour (vector 1 0 0))
+        (translate (vector 0 1 0))
+        (scale 0.6)
+        (draw-sphere)
+        ; eyes
+        (colour (vector 1 1 1))
+        (translate (vector 0 0 1))
+        (scale 0.2)
+        (draw-sphere))
+    ; body
+    (with-state
+        (colour (vector 1 1 0))
+        (translate (vector 0 -.4 0))
+        (scale (vector 1.2 1.6 1))
+        (draw-cube))
+)
+
+; every frame
 (every-frame
-    (draw-user user))
+    (draw-user me))
